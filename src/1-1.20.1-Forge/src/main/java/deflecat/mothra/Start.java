dde8ca89e1171cc944c3a2e92062dc3ce0233276
Rushan Mukhutdinov <firvelfrivol@gmail.com>
2026-09-25 01:20:30 +0300

    A1
    * Подкорректированы приписки в build.gradle,
    * подавлены предупреждения в build.gradle,
    * продолжена разработка поддержки версионирования 40A в начальном классе Start.java.

--- src/1-1.20.1-Forge/src/main/java/deflecat/mothra/Start.java
+++ src/1-1.20.1-Forge/src/main/java/deflecat/mothra/Start.java
@@ -16,6 +16,19 @@ public class Start {
     // static — это блок, что срабатывает в момент первой загрузки класса.
     // Я бы мог вместо него использовать конструктор, но я выбрал static, так как в будущем возможно буду обращаться сюда через миксины, чтобы получить версию мода в формате 40А.
     static {
+        // Как бы я не хотел не использовать здесь try, на это жалуется ИСР… ведь работа с файлами опа$$$на.
+        // В скобках try я создаю поток из текста mothra.properties.
+        // От применения try, конечно, есть некоторая польза. Он автоматически закрывает поток по завершению чтения и при ошибке. Но, я, что, как будто не мог… вручную первое прописать?
+        try (InputStream stream = Start.class.getResourceAsStream("mothra.properties")) {
+            // Это читалка для ранее загруженного как поток mothra.properties
+            Properties props = new Properties();
 
+            // Загрузка mothra.properties в props для чтения
+            props.load(stream);
+
+            // И загрузка значений с mothra.properties в местные переменные
+            semverVersion = props.getProperty("semver_version", semverVersion);
+            sorokaVersion = props.getProperty("40a_version", sorokaVersion);
+        } catch (Exception ignored) {}
     }
 }
